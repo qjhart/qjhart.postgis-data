b8ff2d1f0d5fe28729a07206034adca9a35f507e
Quinn Hart <qjhart@ucdavis.edu>
2012-01-19 13:17:37 -0800

    Updated zipcodes to use 2000 and 2010 data both

--- zipcode/zipcode.mk
+++ zipcode/zipcode.mk
@@ -1,5 +1,5 @@
 #! /usr/bin/make -f
-# This Makefile is designed to be included, in a more comprehenisve makefile.
+# This Makefile is designed to be included, in a more comprehensive makefile.
 
 ifndef configure.mk
 include ../configure.mk
@@ -9,17 +9,19 @@ endif
 zipcode.mk:=1
 
 # Only 2000 and 2010 exist
-year:=2010
+yr:=00 10
 states:=06
 
-zipcode.data:=$(patsubst %,db/zipcode.zip5.%,${states})
+down:=ftp2.census.gov/geo/tiger/TIGER2010/ZCTA5
+zipcode.data:=$(foreach y,${yr},$(patsubst %,db/zipcode.zip5.$y.%,${states}))
 
-down:=ftp2.census.gov/geo/tiger/TIGER2010/ZCTA5/${year}
+zipcode.zips:=$(foreach y,${yr},$(patsubst %,${down}/20$y/tl_2010_%_zcta5$y.zip,${states}))
 
 INFO::
-	@echo This makefile recreates zipcodes from CENSUS
-	@echo years: ${years}
+	@echo This makefile recreates zipcodes from CENSUS.
 	@echo db: ${zipcode.data}
+	@echo zipfiles: ${zipcode.zips}
+	@echo run with make db
 
 .PHONY:db 
 db:: db/zipcode ${zipcode.data}
@@ -32,18 +34,18 @@ db/zipcode:
 clean:
 	rm -rf ${down}
 
-mirror:ftp:=ftp://${down}
 mirror:
-	wget -m $(patsubst %,${ftp}/tl_${year}_%_zcta510.zip,${states})
+	wget -m $(patsubst %,ftp://%,${zipcode.zips})
 
 ${zipcode.data}:db/zipcode.zip5.%:db/zipcode 
 	${PG} -c 'truncate zipcode.zip5;';
-	for f in $(patsubst %,${down}/tl_${year}_%_zcta510.zip,${states}); do \
+	for f in ${zipcode.zips}; do \
 	  b=`basename $$f .zip`; \
+	  yr=$${b#*zcta5}; \
 	  unzip $$f; \
 	  ${shp2pgsql} -D -d -s 4269 $$b.shp zipcode.temp | ${PG} > /dev/null; \
-	  ${PG} -c 'insert into zipcode.zip5 (zipcode,geoid,boundary) select zcta5ce10,geoid10,transform(geom,${srid}) as boundary from zipcode.temp'; \
-	  ${PG} -c "drop table zipcode.temp"; \
+	  ${PG} -c "insert into zipcode.zip5 (year,zipcode,geoid,boundary) select 2000+$${yr},zcta5ce$${yr},geoid$${yr},transform(the_geom,${srid}) as boundary from zipcode.temp"; \
+#	  ${PG} -c "drop table zipcode.temp"; \
 	  rm $$b.*; \
 	done
 	touch $@
--- zipcode/zipcode.sql
+++ zipcode/zipcode.sql
@@ -7,8 +7,11 @@ SET STANDARD_CONFORMING_STRINGS TO ON;
 
 CREATE TABLE zip5 (
 gid serial primary key,
-zipcode varchar(5) unique,
-geoid varchar(10) unique
+year integer not null,
+zipcode varchar(5),
+geoid varchar(10),
+unique(year,zipcode),
+unique(year,geoid)
 );
 SELECT AddGeometryColumn('zipcode','zip5','boundary',:srid,'MULTIPOLYGON',2);
 CREATE INDEX "zip5_boundary_gist" ON zip5 USING GIST ("boundary");
