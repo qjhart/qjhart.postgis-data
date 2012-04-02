5cc7474bccea59013da87f08399f6b93f364022f
Quinn Hart <qjhart@ucdavis.edu>
2012-04-02 11:41:47 -0700

    Added averages and DEM to prism

--- prism/prism.sql
+++ prism/prism.sql
@@ -21,7 +21,18 @@ SELECT AddRasterColumn('prism','us','ppt',4322, ARRAY['32BSI'],
        false, false, ARRAY[-9999.0], 0.041666666670000, -0.041666666670000, 
        null, null, null);
 
-create function new_from_template(sch text,rst text,template raster,OUT ok text)
+-- Make static data, but need a default raster.
+CREATE TABLE static (
+ rid serial PRIMARY KEY,
+ layer text unique
+);
+
+select AddRasterColumn('prism','static','rast',srid, ARRAY['32BSI'], 
+       false, false, ARRAY[-9999.0], scalex,scaley, null, null, null)
+from (select (st_metadata(default_rast())).*) as r;
+
+create function new_from_template(sch text,rst text,template raster,
+OUT ok text)
 AS $$
 BEGIN
 EXECUTE 'DROP TABLE IF EXISTS '||sch||'.'||rst||' CASCADE';
@@ -44,32 +55,46 @@ END;
 $$ LANGUAGE PLPGSQL;
 
 
---select new_from_template('prism','avg',default_rast());
---alter table avg rename year to start;
---alter table avg add column stop integer;
+create function create_avg(OUT boolean) 
+as $$
+select new_from_template('prism','avg',default_rast());
+alter table avg rename year to start;
+alter table avg add column stop integer;
+alter table avg drop constraint avg_year_month_key;
+alter table avg add constraint avg_start_stop_month_key 
+  unique(start,stop,month);
+select * from compute_2avg();
+select * from compute_4avg(4);
+select * from compute_4avg(8);
+select * from compute_4avg(16);
+select true;
+$$ LANGUAGE SQL;
+
 
 create function compute_2avg(OUT count integer)
 as $$
-BEGIN;
+BEGIN
 insert into avg (start,stop,month,tmin,tmax,ppt) 
 select r1.year as start,r2.year as stop,r1.month,
 ST_MapAlgebraExpr(r1.tmin,r2.tmin,'((rast1 + rast2)/2.0)::integer') as tmin,
 ST_MapAlgebraExpr(r1.tmax,r2.tmax,'((rast1 + rast2)/2.0)::integer') as tmax,
 ST_MapAlgebraExpr(r1.ppt,r2.ppt,'((rast1 + rast2)/2.0)::integer') as ppt
-from pnw r1 join pnw r2 on (r1.year=r2.year-1 and r1.month=r2.month);
+from climate r1 join climate r2 on (r1.year=r2.year-1 and r1.month=r2.month);
 select into count count(*) from avg;
 END;
 $$ LANGUAGE plpgsql;
 
-create function compute_4avg(OUT count integer)
+create or replace function compute_4avg(diff integer,OUT count integer)
 as $$
-BEGIN;
+BEGIN
+--delete from avg where (1+stop-start)=diff;
 insert into avg (start,stop,month,tmin,tmax,ppt) 
 select r1.start as start,r2.stop as stop,r1.month,
 ST_MapAlgebraExpr(r1.tmin,r2.tmin,'((rast1 + rast2)/2.0)::integer') as tmin,
 ST_MapAlgebraExpr(r1.tmax,r2.tmax,'((rast1 + rast2)/2.0)::integer') as tmax,
 ST_MapAlgebraExpr(r1.ppt,r2.ppt,'((rast1 + rast2)/2.0)::integer') as ppt
-from avg r1 join avg r2 on (r1.stop=r2.start and r1.month=r2.month);
-select into count count(*) from avg;
+from avg r1 join avg r2 on (r1.stop=r2.start-1 and r1.month=r2.month
+and 1+r2.stop-r1.start=diff);
+select into count count(*) from avg where 1+stop-start=diff;
 END;
 $$ LANGUAGE plpgsql;
