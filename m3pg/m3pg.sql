4184974573d82ed1f81c2b615b43e2e175545c42
Quinn Hart <qjhart@ucdavis.edu>
2012-04-04 15:59:27 -0700

    Added R soil_class

--- m3pg/m3pg.sql
+++ m3pg/m3pg.sql
@@ -15,8 +15,16 @@ copy soil_class from stdin delimiter '|';
 0|NA|No soil water effects|999|9
 1|Sa|sandy|0.7|9
 2|SaLo|sandy loam|0.6|7
-3|CaLo|clay loam|0.5|5
+3|ClLo|clay loam|0.5|5
 4|Cl|clay|0.4|3
+\N|LoSa|loamy sand|0.8|8
+\N|SaClLo|sandy clay loam|0.5|6
+\N|SaCl|sandy clay|0.4|5
+\N|Lo|loam|.55|6
+\N|SiCl|silty clay|.45|4
+\N|SiClLo|silty clay loam|.5|5
+\N|SiLo|silty loam|.5|5
+\N|Si|silt|.5|5
 \.
 
 -- For classes 1-4....
@@ -46,10 +54,28 @@ BEGIN
 END;
 $$ LANGUAGE 'plpgsql';
 
-create or replace function m3pg.soil_class(sand float,silt float,clay float,
-OUT class varchar(8)) AS $$
-BEGIN
-  select into class CASE when (clay>.5) THEN 'Cl' ELSE 'UNKN' END;
-END;
-$$ LANGUAGE 'plpgsql';
+create TYPE SaSiCl as (
+       "SAND" float,
+       "SILT" float,
+       "CLAY" float
+);
+
+create or replace function m3pg.soil_class(indata SaSiCl)
+RETURNS varchar(8) AS $$
+  return(TT.points.in.classes(tri.data=indata,PiC.type="t"))
+$$ LANGUAGE 'plr' STRICT;
+
+-- create or replace function m3pg.soil_class_tridata(indata SaSiCl)
+-- RETURNS tridata AS $$
+--    outdata <- data.frame(SAND=1,SOIL=5,CLAY=80,class='')
+--    outdata$class<-TT.points.in.classes(tri.data=indata,PiC.type="t")
+--    return(outdata)
+-- $$ LANGUAGE 'plr' STRICT;
+
+
+create or replace function m3pg.initialize_R(OUT good boolean) AS $$
+  library(soiltexture)
+  TT.set("class.sys"="USDA.TT")
+  return(TRUE)
+$$ LANGUAGE 'plr' STRICT;
 
