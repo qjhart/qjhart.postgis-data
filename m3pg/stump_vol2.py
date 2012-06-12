2403fd9be578b0ea9d383e00b6909d7696482abf
Peter Tittmann <ptittmann@gmail.com>
2012-06-12 11:26:12 -0700

    allometric equation and plots for determining stump volume

---
+++ m3pg/stump_vol2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 11 18:56:57 2012
+
+@author: peter
+
+3PG outputs biomass/tree we need to determine the biomass of the stump
+to determine this we will use the taper equation from benbrahim and galvand for which 
+the followimg in necessary:
+
+basal diameter
+total tree heigh
+
+to estimate th basal dimaeter we need  to:
+1. use DBH and parameters from benbrahim
+
+"""
+
